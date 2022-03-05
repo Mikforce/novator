ae7b03d1a6039b72202184148efed9bbd94bbcc2
Mikforce <mikforce4378403@gmail.com>
2022-03-05 17:13:34 +0300

    jq ругается
    не скролит при нажатии в шапке , и не в мобильной версии все съезжает с лево... я хз...

--- app.js
+++ app.js
@@ -1,20 +1,38 @@
 $(function() {
-
+/*fixed header*/
 let header = $("#header");
 let intro = $("#intro");
-let introH;
+let introH = intro.innerHeight();
 let skrollPos = $(window).scrollTop();
 
+checkScroll(scrollPos, introH);
 
-$(window).on("scroll load resize", function() {
+$(window).on("scroll resize", function() {
     introH = intro.innerHeight();
     scrollPos = $(this).scrollTop();
 
-if( scrollPos > introH ) {
-    header.addClass("fixed");
-} else {
-    header.removeClass("fixed");
-}
+    checkScroll(scrollPos, introH);
     });
+function checkScroll(scrollPos, introH) {
+    if( scrollPos > introH ) {
+        header.addClass("fixed");
+    } else {
+        header.removeClass("fixed");
+    }
+}
 
-}) ;
+/*smooth scroll */
+
+$("[data-sroll]").on("click", function(event) {
+    event.preventDefault();
+
+    let elementId = $(this).data("scroll");
+    let elementOffset = $(elementId).offset().top;
+
+    $("html, body").animate({
+        scrollTop: elementOffset -70
+},700);
+});
+
+
+});
